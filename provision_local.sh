e5cbe46a28c939feb3af95a4a0429cf2e17013c0
Kevin Heis <heiskr@users.noreply.github.com>
2013-12-13 19:14:32 -0800

    Starting work on local development Vagrant box

---
+++ provision_local.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+#### Initialize ###############################################################
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install language-pack-en
+
+#### Aliases ##################################################################
+
+#### PostgreSQL ###############################################################
+
+sudo apt-get -y install python-dev
+sudo apt-get -y install postgresql-9.1
+sudo apt-get -y install libpq-dev
+
+#### Python ###################################################################
+
+sudo apt-get -y install python-pip
+sudo pip install -r /vagrant/api/requirements.txt
+
+#### Redis ####################################################################
+
+sudo apt-get -y install redis-server
+
+#### Stylus and CoffeeScript ##################################################
+
+sudo apt-get -y install npm
+sudo npm install -g coffee-script stylus
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+echo 'export NODE_PATH=/usr/local/lib/node_modules' >> ~/.bashrc
+
+#### Server ###################################################################
+
+cd /var/www
+sudo apt-get -y install nginx
+sudo uwsgi  --http :8652 \
+            --pythonpath /vagrant/api \
+            --wsgi index:app \
+            --processes 4 \
+            --threads 2 \
+            --daemonize /tmp/uwsgi.log
+sudo nginx -c /vagrant/nginx.conf
+
+echo "Hooray! Provisioned."
