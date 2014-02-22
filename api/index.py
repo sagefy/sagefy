2aa14b7cdda197117a09faaddea3535274192c96
Kevin Heis <heiskr@gmail.com>
2014-02-21 22:10:13 -0800

    Add welcome message to Sagefy API

--- api/index.py
+++ api/index.py
@@ -3,5 +3,14 @@ import controllers.user
 import controllers.notification
 import controllers.message
 
+
+@app.route('/api/')
+def api_index():
+    """
+    View a documentation page.
+    """
+    return 'Welcome to the Sagefy API.'
+
+
 if __name__ == '__main__':
     app.run()
