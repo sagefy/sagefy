1fb690906785600460cf7df1c6e6f775eadc7f6b
Kevin Heis <heiskr@users.noreply.github.com>
2014-04-30 15:29:58 -0700

    Upgrade IDs to length 16

--- api/modules/util.py
+++ api/modules/util.py
@@ -6,9 +6,9 @@ import random
 import string
 
 
-def uniqid(length=9):
+def uniqid(length=16):
     """
-    Generates a unique string with 9 characters.
+    Generates a unique string with 16 characters.
     http://stackoverflow.com/a/2257449
     """
     return ''.join(
