35a0c3f65083556db839b3426a56579d1b7766e3
Kevin Heis <heiskr@users.noreply.github.com>
2014-05-10 19:49:37 -0700

    Generate logo with code fixes #22

--- ui/resources/astrolabe.py
+++ ui/resources/astrolabe.py
@@ -32,53 +32,15 @@ attrs = {
         },
 
         "buttresses": {
-            1: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
-
-            2: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
-
-            3: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
-
-            4: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
-
-            5: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
-
-            6: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
-
-            7: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
-
-            8: {
-                "width": 5.0,
-                "height": 5.0,
-                "fill": dark_color,
-            },
+            "fill": dark_color,
+            1: {"rotate": '0'},
+            2: {"rotate": '0'},
+            3: {"rotate": '90'},
+            4: {"rotate": '90'},
+            5: {"rotate": '180'},
+            6: {"rotate": '180'},
+            7: {"rotate": '270'},
+            8: {"rotate": '270'},
         }
     },
 
@@ -117,14 +79,10 @@ attrs = {
 
         "arms": {
             1: {
-                "width": 5.0 / 2,
-                "slice": 0.5,
                 "fill": light_color,
             },
 
             2: {
-                "width": 5.0 / 2,
-                "slice": 0.5,
                 "fill": light_color,
             },
         },
@@ -210,10 +168,194 @@ def get_attrs(base):
         attrs['main_circle']['cy']
         - attrs['measure']['height'] / 2
     )
+    arms_width = (
+        attrs['main_circle']['r']
+        - attrs['measure']['width'] / 2
+    )
+    arms_y_center = (
+        attrs['main_circle']['pipes'][1]['y1']
+    )
+    arms1_x1 = (
+        attrs['main_circle']['cx']
+        + attrs['measure']['width'] / 2
+    )
+    arms2_x1 = (
+        attrs['main_circle']['cx']
+        - attrs['measure']['width'] / 2
+    )
+    attrs['measure']['arms'][1]['d'] = \
+        'M %s %s L %s %s Q %s %s %s %s' % (
+            # x1
+            base * arms1_x1,
+            # y1
+            base * arms_y_center,
+            # x2
+            base * (arms1_x1 + arms_width),
+            # y2
+            base * arms_y_center,
+            # xq
+            base * (arms1_x1 + arms_width / 2),
+            # yq
+            base * (
+                arms_y_center
+                - attrs['measure']['height'] / 2
+                + attrs['measure']['radius']
+            ),
+            # x3
+            base * arms1_x1,
+            # y3
+            base * (
+                arms_y_center
+                - attrs['measure']['height'] / 2
+                + attrs['measure']['radius']
+            ),
+        )
+    attrs['measure']['arms'][2]['d'] = \
+        'M %s %s L %s %s Q %s %s %s %s' % (
+            # x1
+            base * arms2_x1,
+            # y1
+            base * arms_y_center,
+            # x2
+            base * (arms2_x1 - arms_width),
+            # y2
+            base * arms_y_center,
+            # xq
+            base * (arms2_x1 - arms_width / 2),
+            # yq
+            base * (
+                arms_y_center
+                + attrs['measure']['height'] / 2
+                - attrs['measure']['radius']
+            ),
+            # x3
+            base * arms2_x1,
+            # y3
+            base * (
+                arms_y_center
+                + attrs['measure']['height'] / 2
+                - attrs['measure']['radius']
+            )
+        )
 
-    # TODO: measure.arms[1, 2].d
-    # TODO: main_circle.buttresses[1..8].d
-    # TODO: small_circle.buttresses[1, 2].d
+    s = attrs['main_circle']['pipes'][2]['stroke_width'] / 2
+    b = 5.0
+
+    base_path = \
+        'M %s %s L %s %s Q %s %s %s %s L %s %s' % (
+            # x1
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s),
+            # y1
+            base * (attrs['main_circle']['pipes'][1]['y1'] - s),
+            # x2
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s + b),
+            # y2
+            base * (attrs['main_circle']['pipes'][1]['y1'] - s) + 1,
+            # xq
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s),
+            # yq
+            base * (attrs['main_circle']['pipes'][1]['y1'] - s),
+            # x3
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s) + base * 1.5,
+            # y3
+            base * (attrs['main_circle']['pipes'][1]['y1'] - s - b),
+            # x4
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s),
+            # y4
+            base * (attrs['main_circle']['pipes'][1]['y1'] - s - b),
+        )
+
+    base_path2 = \
+        'M %s %s L %s %s Q %s %s %s %s L %s %s' % (
+            # x1
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s),
+            # y1
+            base * (attrs['main_circle']['pipes'][1]['y1'] + s),
+            # x2
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s + b),
+            # y2
+            base * (attrs['main_circle']['pipes'][1]['y1'] + s) - 1,
+            # xq
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s),
+            # yq
+            base * (attrs['main_circle']['pipes'][1]['y1'] + s),
+            # x3
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s) + base * 1.5,
+            # y3
+            base * (attrs['main_circle']['pipes'][1]['y1'] + s + b),
+            # x4
+            base * (attrs['main_circle']['pipes'][1]['x1'] + s),
+            # y4
+            base * (attrs['main_circle']['pipes'][1]['y1'] + s + b),
+        )
+
+    attrs['main_circle']['buttresses']['d'] = base_path
+    attrs['main_circle']['buttresses']['d2'] = base_path2
+
+    scbx1 = (
+        attrs['width'] / 2
+        - attrs['small_circle']['neck']['stroke_width'] / 2
+    )
+
+    scby1 = (attrs['height'] - (
+        attrs['main_circle']['pipes'][2]['stroke_width']
+        + attrs['main_circle']['r'] * 2
+    ))
+
+    attrs['small_circle']['buttresses'][1]['d'] = \
+        'M %s %s L %s %s Q %s %s %s %s' % (
+            # x1
+            base * scbx1,
+            # y1
+            base * scby1,
+            # x2
+            base * scbx1 + 1,
+            # y2
+            base * (
+                scby1
+                - attrs['small_circle']['neck']['height']
+                + attrs['main_circle']['pipes'][2]['stroke_width'] / 2
+                + attrs['small_circle']['stroke_width'] / 2
+            ),
+            # xq
+            base * scbx1,
+            # yq
+            base * scby1,
+            # x3
+            base * scbx1 - base * 5 + 1,
+            # y3
+            base * scby1 + base * 1.3
+        )
+
+    scbx2 = (
+        attrs['width'] / 2
+        + attrs['small_circle']['neck']['stroke_width'] / 2
+    )
+
+    attrs['small_circle']['buttresses'][2]['d'] = \
+        'M %s %s L %s %s Q %s %s %s %s' % (
+            # x1
+            base * scbx2,
+            # y1
+            base * scby1,
+            # x2
+            base * scbx2 - 1,
+            # y2
+            base * (
+                scby1
+                - attrs['small_circle']['neck']['height']
+                + attrs['main_circle']['pipes'][2]['stroke_width'] / 2
+                + attrs['small_circle']['stroke_width'] / 2
+            ),
+            # xq
+            base * scbx2,
+            # yq
+            base * scby1,
+            # x3
+            base * scbx2 + base * 5 - 1,
+            # y3
+            base * scby1 + base * 1.3
+        )
 
     def baseify(ats):
         for key, val in ats.iteritems():
