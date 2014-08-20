39f7c2309af1d7573eb9070933e1f7444cac85da
Kevin Heis <heiskr@users.noreply.github.com>
2014-08-19 17:58:36 -0700

    Working on ODM embedding tests #74

--- api/tests/test_odm_embed.py
+++ api/tests/test_odm_embed.py
@@ -1,105 +1,288 @@
-from odm.model import Field, Document, Model
-from odm.embed import has, has_many
-from odm.validations import required, boolean, email, minlength
-import pytest
-
-
-def encrypt_password(field):
-    return '$2a$' + field.get()
-
-
-class Settings(Document):
-    email_notifications = Field(
-        validations=(boolean,),
-        default=False,
-    )
-
-
-def is_current_user():
-    return True
-
-
-class User(Model):
-    tablename = 'users'
-    name = Field(
-        validations=(required,),
-        unique=True
-    )
-    email = Field(
-        validations=(required, email),
-        access='private',
-        unique=True
-    )
-    password = Field(
-        validations=(required, (minlength, 8)),
-        access=False,
-        before_save=encrypt_password
-    )
-    settings = has(Settings)
-
-    def is_current_user(self):
-        return is_current_user()
-
-
-class Book(Document):
-    name = Field(
-        validations=(required,),
-        default='Untitled'
-    )
-
-
-class Author(User):
-    book = has_many(Book)
-
-
-@pytest.mark.xfail
-def test_extend(app, db_conn, users_table):
-    """
-    Expect a model to be extendable.
-    """
-    author = Author.insert({
-        'name': 'test',
-        'email': 'test@example.com',
-        'password': 'abcd1234',
-    })
-    assert isinstance(author, User)
-    assert isinstance(author, Author)
-    assert author.name.get() == 'test'
-
-
-@pytest.mark.xfail
-def test_embed_many(app, db_conn, users_table):
-    """
-    Expect a model to embed many documents.
-    """
-    author = Author.insert({
-        'name': 'test',
-        'email': 'test@example.com',
-        'password': 'abcd1234',
-        'books': [
-            {
-                'name': 'sunrise'
-            },
-            {
-                'name': 'sunset'
-            }
-        ]
-    })
-    assert author.books.get(0).name.get() == 'sunrise'
-    assert author.books.get(1).name.get() == 'sunset'
-
-
-@pytest.mark.xfail
 def test_embed(app, db_conn, users_table):
     """
-    Expect a model to embed a document.
+    Expect to embed a document on a model.
     """
-    user = User.insert({
-        'name': 'test',
-        'email': 'test@example.com',
-        'password': 'abcd1234',
-        'settings': {
-            'email_notifications': True
-        }
-    })
-    assert user.settings.email_notifications is True
+    assert False
+
+
+def test_embed_many(app, db_conn, users_table):
+    """
+    Expect to embed a list of documents on a model.
+    """
+    assert False
+
+
+def test_embed_get(app, db_conn, users_table):
+    """
+    Expect to get a document embedded.
+    """
+    assert False
+
+
+def test_embed_many_get(app, db_conn, users_table):
+    """
+    Expect to get a list of documents embedded.
+    """
+    assert False
+
+
+def test_embed_default(app, db_conn, users_table):
+    """
+    Expect to get defaults on embedded documents.
+    """
+    assert False
+
+
+def test_embed_many_default(app, db_conn, users_table):
+    """
+    Expect to get defaults on lists of embedded documents.
+    """
+    assert False
+
+
+def test_embed_set(app, db_conn, users_table):
+    """
+    Expect to set fields on embedded document.
+    """
+    assert False
+
+
+def test_embed_many_set(app, db_conn, users_table):
+    """
+    Expect to set fields on list embedded document.
+    """
+    assert False
+
+
+def test_embed_update(app, db_conn, users_table):
+    """
+    Expect to update fields on embedded document.
+    """
+    assert False
+
+
+def test_embed_many_update(app, db_conn, users_table):
+    """
+    Expect to update fields on list embedded document.
+    """
+    assert False
+
+
+def test_embed_append(app, db_conn, users_table):
+    """
+    Expect to add a new embedded document to list.
+    """
+    assert False
+
+
+def test_embed_splice(app, db_conn, users_table):
+    """
+    Expect to remove an embedded document from a list.
+    """
+    assert False
+
+
+def test_embed_before(app, db_conn, users_table):
+    """
+    Expect before_save called on embedded document.
+    """
+    assert False
+
+
+def test_embed_fields_before(app, db_conn, users_table):
+    """
+    Expect before_save called on embedded document fields.
+    """
+    assert False
+
+
+def test_embed_many_before(app, db_conn, users_table):
+    """
+    Expect before_save called on list of embedded documents.
+    """
+    assert False
+
+
+def test_embed_many_fields_before(app, db_conn, users_table):
+    """
+    Expect before_save called on list of embedded documents fields.
+    """
+    assert False
+
+
+def test_access(app, db_conn, users_table):
+    """
+    Expect access to trickle down to embedded document.
+    """
+    assert False
+
+
+def test_access_fields(app, db_conn, users_table):
+    """
+    Expect access to trickle down to embedded document fields.
+    """
+    assert False
+
+
+def test_access_many(app, db_conn, users_table):
+    """
+    Expect access to trickle down to lists of embedded documents.
+    """
+    assert False
+
+
+def test_access_many_fields(app, db_conn, users_table):
+    """
+    Expect access to trickle down to lists of embedded document fields.
+    """
+    assert False
+
+
+def test_validation(app, db_conn, users_table):
+    """
+    Expect validation on embedded document.
+    """
+    assert False
+
+
+def test_validation_fields(app, db_conn, users_table):
+    """
+    Expect validation on embedded document fields.
+    """
+    assert False
+
+
+def test_validation_many(app, db_conn, users_table):
+    """
+    Expect validation on list of embedded documents.
+    """
+    assert False
+
+
+def test_validation_many_fields(app, db_conn, users_table):
+    """
+    Expect validation on list of embedded document fields.
+    """
+    assert False
+
+
+def test_id(app, db_conn, users_table):
+    """
+    Expect only one ID for an entire model.
+    """
+    assert False
+
+
+def test_created(app, db_conn, users_table):
+    """
+    Expect only one created time for an entire model.
+    """
+    assert False
+
+
+def test_modified(app, db_conn, users_table):
+    """
+    Expect only one modified time for an entire model.
+    """
+    assert False
+
+
+def test_sync(app, db_conn, users_table):
+    """
+    Expect to synchronize an embedded document.
+    """
+    assert False
+
+
+def test_sync_many(app, db_conn, users_table):
+    """
+    Expect to synchronize a list of embedded documents.
+    """
+    assert False
+
+
+def test_get_db(app, db_conn, users_table):
+    """
+    Expect to get a model with an embedded document from the database.
+    """
+    assert False
+
+
+def test_get_db_many(app, db_conn, users_table):
+    """
+    Expect to get a model with a list of embedded document from the database.
+    """
+    assert False
+
+
+def test_list_db(app, db_conn, users_table):
+    """
+    Expect to list models with embedded documents from the database.
+    """
+    assert False
+
+
+def test_list_db_many(app, db_conn, users_table):
+    """
+    Expect to list models with lists of embedded documents from the database.
+    """
+    assert False
+
+
+def test_insert_db(app, db_conn, users_table):
+    """
+    Expect to insert a model with an embedded document into the database.
+    """
+    assert False
+
+
+def test_insert_db_many(app, db_conn, users_table):
+    """
+    Expect to insert a model with a list of embedded document
+    into the database.
+    """
+    assert False
+
+
+def test_update(app, db_conn, users_table):
+    """
+    Expect to update a model with an embedded document into the database.
+    """
+    assert False
+
+
+def test_update_many(app, db_conn, users_table):
+    """
+    Expect to update a model with a list of embedded document
+    into the database.
+    """
+    assert False
+
+
+def test_delete(app, db_conn, users_table):
+    """
+    Expect to delete a model with an embedded document from the database.
+    """
+    assert False
+
+
+def test_delete_many(app, db_conn, users_table):
+    """
+    Expect to delete a model with a list of embedded document
+    from the database.
+    """
+    assert False
+
+
+def test_unique(app, db_conn, users_table):
+    """
+    Expect unique to check embedded document fields.
+    """
+    assert False
+
+
+def test_unique_many(app, db_conn, users_table):
+    """
+    Expect unique to check list embedded document fields.
+    """
+    assert False
