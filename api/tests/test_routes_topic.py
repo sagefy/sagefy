817d48ebbd9a0edd7da317d4efe3a4253079bfe8
Kevin Heis <heiskr@users.noreply.github.com>
2014-11-12 12:56:04 -0800

    API Unit Test > Routes > Topics > Create Post fixes #163

--- api/tests/test_routes_topic.py
+++ api/tests/test_routes_topic.py
@@ -3,6 +3,25 @@ import pytest
 xfail = pytest.mark.xfail
 
 
+from test_routes_user import create_user_in_db, login, logout
+import json
+import rethinkdb as r
+
+
+def create_topic_in_db(topics_table, db_conn):
+    topics_table.insert({
+        'id': 'wxyz7890',
+        'created': r.now(),
+        'modified': r.now(),
+        'language': 'en',
+        'name': 'A Modest Proposal',
+        'entity': {
+            'entity_id': 'efgh5678',
+            'kind': 'unit'
+        }
+    }).run(db_conn)
+
+
 @xfail
 def test_search(app, db_conn):
     """
@@ -172,43 +191,131 @@ def test_get_posts_votes(app, db_conn):
 
 
 @xfail
-def test_create_post(app, db_conn):
+def test_create_post(app, db_conn, users_table, topics_table, posts_table):
     """
     Expect create post.
     """
-    return False
+    create_user_in_db(users_table, db_conn)
+    create_topic_in_db(topics_table, db_conn)
+    with app.test_client() as c:
+        login(c)
+        response = c.post('/api/topics/wxyz7890/posts', data=json.dumps({
+            # Should default to > 'kind': 'post',
+            'body': '''A Modest Proposal for Preventing the Children of Poor
+                People From Being a Burthen to Their Parents or Country, and
+                for Making Them Beneficial to the Publick.''',
+        }), content_type='application/json')
+        assert response.status_code == 200
+        data = json.loads(response.data.decode('utf-8'))
+        assert 'Beneficial to the Publick' in data['post']['body']
+        logout(c)
 
 
 @xfail
-def test_create_post_errors(app, db_conn):
+def test_create_post_errors(app, db_conn, users_table, topics_table,
+                            posts_table):
     """
     Expect create post missing field to show errors.
     """
-    return False
+    create_user_in_db(users_table, db_conn)
+    create_topic_in_db(topics_table, db_conn)
+    with app.test_client() as c:
+        login(c)
+        response = c.post('/api/topics/wxyz7890/posts',
+                          data=json.dumps({}),
+                          content_type='application/json')
+        assert response.status_code == 400
+        data = json.loads(response.data.decode('utf-8'))
+        assert 'errors' in data
+        logout(c)
 
 
 @xfail
-def test_create_post_login(app, db_conn):
+def test_create_post_login(app, db_conn, users_table, topics_table,
+                           posts_table):
     """
     Expect create post to require login.
     """
-    return False
+    create_topic_in_db(topics_table, db_conn)
+    with app.test_client() as c:
+        response = c.post('/api/topics/wxyz7890/posts', data=json.dumps({
+            # Should default to > 'kind': 'post',
+            'body': '''A Modest Proposal for Preventing the Children of Poor
+                People From Being a Burthen to Their Parents or Country, and
+                for Making Them Beneficial to the Publick.''',
+        }), content_type='application/json')
+        assert response.status_code == 401
+        data = json.loads(response.data.decode('utf-8'))
+        assert 'errors' in data
 
 
 @xfail
-def test_create_post_proposal(app, db_conn):
+def test_create_post_proposal(app, db_conn, users_table, topics_table,
+                              posts_table):
     """
     Expect create post to create a proposal.
     """
-    return False
+    create_user_in_db(users_table, db_conn)
+    create_topic_in_db(topics_table, db_conn)
+    with app.test_client() as c:
+        login(c)
+        response = c.post('/api/topics/wxyz7890/posts', data=json.dumps({
+            'kind': 'proposal',
+            'name': 'New Unit',
+            'body': '''A Modest Proposal for Preventing the Children of Poor
+                People From Being a Burthen to Their Parents or Country, and
+                for Making Them Beneficial to the Publick.''',
+            'action': 'create',
+            'unit': {
+                'name': 'Satire',
+                'body': '''Learn the use of humor, irony, exaggeration, or
+                ridicule to expose and criticize people's
+                stupidity or vices.''',
+                'tags': ['literature']
+            },
+        }), content_type='application/json')
+        assert response.status_code == 200
+        data = json.loads(response.data.decode('utf-8'))
+        assert data['post']['kind'] == 'proposal'
+        logout(c)
 
 
 @xfail
-def test_create_post_vote(app, db_conn):
+def test_create_post_vote(app, db_conn, users_table, topics_table,
+                          posts_table):
     """
     Expect create post to create a vote.
     """
-    return False
+    create_user_in_db(users_table, db_conn)
+    create_topic_in_db(topics_table, db_conn)
+    posts_table.insert({
+        'id': 'jklm',
+        'created': r.now(),
+        'modified': r.now(),
+        'language': 'en',
+        'user_id': 'abcd1234',
+        'topic_id': 'wxyz7890',
+        'body': '''A Modest Proposal for Preventing the Children of Poor
+            People From Being a Burthen to Their Parents or Country, and
+            for Making Them Beneficial to the Publick.''',
+        'kind': 'proposal',
+        'entity_version_id': '1',
+        'name': 'New Unit',
+        'status': 'pending',
+        'action': 'create'
+    }).run(db_conn)
+    with app.test_client() as c:
+        login(c)
+        response = c.post('/api/topics/wxyz7890/posts', data=json.dumps({
+            'kind': 'vote',
+            'body': 'Hooray!',
+            'proposal_id': 'jklm',
+            'response': True,
+        }), content_type='application/json')
+        assert response.status_code == 200
+        data = json.loads(response.data.decode('utf-8'))
+        assert data['post']['kind'] == 'vote'
+        logout(c)
 
 
 @xfail
