76c5ee8b2798f1a00d1b6305c8e79a0344d9deb4
Kevin Heis <heiskr@users.noreply.github.com>
2015-04-06 19:57:05 -0700

    ...

---
+++ api/tests/test_modules_sequencer_formulas.py
@@ -0,0 +1,75 @@
+import pytest
+
+xfail = pytest.mark.xfail
+
+
+@xfail
+def test_update(app):
+    """
+    Expect to update entity information based on learner response.
+    """
+
+    assert False
+
+
+@xfail
+def test_correct(app):
+    """
+    Expect to calculate probability of correct.
+    """
+
+    assert False
+
+
+@xfail
+def test_incorrect(app):
+    """
+    Expect to calculate probability of incorrect.
+    """
+
+    assert False
+
+
+@xfail
+def test_difficulty(app):
+    """
+    Expect to calculate average difficulty of card.
+    """
+
+    assert False
+
+
+@xfail
+def test_guess(app):
+    """
+    Expect to update card guess based on learner response.
+    """
+
+    assert False
+
+
+@xfail
+def test_slip(app):
+    """
+    Expect to update card slip based on learner response.
+    """
+
+    assert False
+
+
+@xfail
+def test_belief(app):
+    """
+    Expect to update belief based on learner response time.
+    """
+
+    assert False
+
+
+@xfail
+def test_learned(app):
+    """
+    Expect to update learned based on learner response.
+    """
+
+    assert False
