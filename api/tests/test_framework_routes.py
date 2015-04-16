c5684c6803783f85988363a49e8529ca10aeb8d2
Kevin Heis <heiskr@users.noreply.github.com>
2015-04-15 19:48:20 -0700

    Adding routing tests

--- api/tests/test_framework_routes.py
+++ api/tests/test_framework_routes.py
@@ -2,65 +2,128 @@ import pytest
 
 xfail = pytest.mark.xfail
 
+import framework.routes as routes
+import re
+from framework.routes import get, post, put, delete, abort, \
+    build_path_pattern, find_path
 
-@xfail
+
 def test_get():
     """
     Expect to add a handler to GET.
     """
 
-    assert False
+    start_ln = len(routes.routes['GET'])
+
+    @get('/api/foo')
+    def foo_route(request):
+        return 200, ''
+
+    for path, fn in routes.routes['GET']:
+        if fn == foo_route:
+            found = (path, fn)
+    assert found
+    routes.routes['GET'].remove(found)
+    assert len(routes.routes['GET']) == start_ln
 
 
-@xfail
 def test_post():
     """
     Expect to add a handler to POST.
     """
 
-    assert False
+    start_ln = len(routes.routes['POST'])
+
+    @post('/api/foo')
+    def foo_route(request):
+        return 200, ''
+
+    for path, fn in routes.routes['POST']:
+        if fn == foo_route:
+            found = (path, fn)
+    assert found
+    routes.routes['POST'].remove(found)
+    assert len(routes.routes['POST']) == start_ln
 
 
-@xfail
 def test_put():
     """
     Expect to add a handler to PUT.
     """
 
-    assert False
+    start_ln = len(routes.routes['PUT'])
+
+    @put('/api/foo')
+    def foo_route(request):
+        return 200, ''
+
+    for path, fn in routes.routes['PUT']:
+        if fn == foo_route:
+            found = (path, fn)
+    assert found
+    routes.routes['PUT'].remove(found)
+    assert len(routes.routes['PUT']) == start_ln
 
 
-@xfail
 def test_delete():
     """
     Expect to add a handler to DELETE.
     """
 
-    assert False
+    start_ln = len(routes.routes['DELETE'])
+
+    @delete('/api/foo')
+    def foo_route(request):
+        return 200, ''
+
+    for path, fn in routes.routes['DELETE']:
+        if fn == foo_route:
+            found = (path, fn)
+    assert found
+    routes.routes['DELETE'].remove(found)
+    assert len(routes.routes['DELETE']) == start_ln
 
 
-@xfail
 def test_build_path_pattern():
     """
     Expect to build a path pattern.
     """
 
-    assert False
+    assert (build_path_pattern('/foo') ==
+            re.compile('^/foo/?$'))
+    assert (build_path_pattern('/foo/{u_id}') ==
+            re.compile('^/foo/(?P<u_id>[\w\-]+)/?$'))
+    assert (build_path_pattern('/foo/{u_id}/aaa/{n_id}') ==
+            re.compile('^/foo/(?P<u_id>[\w\-]+)/aaa/(?P<n_id>[\w\-]+)/?$'))
 
 
-@xfail
 def test_find_path():
     """
     Find a handler matching a path.
     """
 
-    assert False
+    start_ln = len(routes.routes['GET'])
+
+    @get('/api/foo/{u_id}')
+    def foo_route(request):
+        return 200, ''
+
+    fn, params = find_path('GET', '/api/foo/a1')
+
+    assert fn == foo_route
+    assert params == {'u_id': 'a1'}
+
+    path = re.compile('^/api/foo/(?P<u_id>[\w\-]+)/?$')
+    routes.routes['GET'].remove((path, fn))
+    assert len(routes.routes['GET']) == start_ln
 
 
-@xfail
 def test_abort():
     """
     Expect to return a standard fail status.
     """
 
-    assert False
+    code, response = abort(404)
+    assert code == 404
+    assert 'errors' in response
+    assert response['errors'][0]['message'] == '404 Not Found'
