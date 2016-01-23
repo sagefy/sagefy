e6d8e07284a5f423d9d55577acf09497cd8b1042
Kevin Heis <heiskr@users.noreply.github.com>
2016-01-23 11:25:11 -0800

    Reading security documents

--- setup/provision_local.sh
+++ setup/provision_local.sh
@@ -35,7 +35,8 @@ sudo apt-get -y install rethinkdb
 sudo chmod -R 777 /var/run
 sudo cp /var/www/setup/rethinkdb.conf /etc/rethinkdb/instances.d/instance1.conf
 sudo /etc/init.d/rethinkdb restart
-# TODO-0 secure
+# Securing RethinkDB: https://rethinkdb.com/docs/security/
+# TODO-1 Use socks to access the admin UI
 
 #### Elasticsearch ############################################################
 
@@ -47,27 +48,31 @@ sudo apt-get -y install elasticsearch
 sudo update-rc.d elasticsearch defaults 95 10
 sudo /etc/init.d/elasticsearch start
 cd /var/www
-# TODO-0 secure
+# Securing ElasticSearch: do.co/1JsVB2O
+# TODO-0 network.bind_host
+# TODO-0 script.disable_dynamic
 
 #### Kibana ###################################################################
 
-cd ~
-wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.0-beta3.tar.gz
-tar xvf kibana-4.0.0-beta3.tar.gz
-sudo mkdir -p /var/www/kibana
-sudo cp -R ~/kibana-4.0.0-beta3/* /var/www/kibana/
-rm kibana-4.0.0-beta3.tar.gz
-rm -R kibana-4.0.0-beta3
-cd /var/www/kibana
-ulimit -v unlimited
+# TODO-2 Enable Kibana
+# cd ~
+# wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.0-beta3.tar.gz
+# tar xvf kibana-4.0.0-beta3.tar.gz
+# sudo mkdir -p /var/www/kibana
+# sudo cp -R ~/kibana-4.0.0-beta3/* /var/www/kibana/
+# rm kibana-4.0.0-beta3.tar.gz
+# rm -R kibana-4.0.0-beta3
+# cd /var/www/kibana
+# ulimit -v unlimited
 # TO RUN: ./bin/kibana
-cd /var/www
-# TODO-0 secure
+# cd /var/www
 
 #### Redis ####################################################################
 
+# Securing Redis: do.co/1nfZxt2
 sudo apt-get -y install redis-server
-# TODO-0 secure
+# TODO-0 Renaming Dangerous Commands
+# TODO-0 Setting Data Directory Ownership and File Permissions
 
 #### Client Tooling ###########################################################
 
@@ -94,6 +99,13 @@ sleep 1
 sudo nginx -s stop
 sleep 1
 sudo nginx -c /var/www/setup/nginx.conf
+
+# TODO-1 Setup file system monitoring do.co/1GfDGYN
+# TODO-1 Setup Digital Ocean private networking do.co/1nrFJE3
+# TODO-1 Setup loggly
+# TODO-1 Setup automated backups (especially of RethinkDB)
+# TODO-1 set up auto updating on ubuntu
+# TODO-2 Re-read bit.ly/1mERm9F
 
 echo "Hooray! Provisioned."
 echo "For script and style watching, run gulp."
