ea6edbe4fe14892147765fa50c3f1c6e0f591c09
Kevin Heis <heiskr@users.noreply.github.com>
2016-10-27 21:09:52 -0700

    Revert tests

--- client/test/modules/events.js
+++ client/test/modules/events.js
@@ -1,1 +1,72 @@
-events = require('../../app/modules/events')const {expect} = require('chai')describe('events', () => {    it.skip('needs tests', () =>        expect(false).to.be.true    })})
+const Events = require('../../app/modules/events')
+
+let e
+let ting
+let f
+
+describe('Events', function () {
+    beforeEach(function () {
+        e = new Events()
+        ting = false
+        f = function (a) {
+            ting = a || true
+        }
+    })
+
+    after(function () {
+        e = null
+    })
+
+    it('should register an event', function () {
+        e.on('poke', f)
+        expect(e.events.poke).to.be.an('array')
+        expect(e.events.poke[0]).to.be.a('function')
+    })
+
+    it('should register only one binding per event', function () {
+        e.on('poke', f)
+        e.on('poke', f)
+        expect(e.events.poke).to.have.length(1)
+    })
+
+    it('should emit an event', function () {
+        e.on('poke', f)
+        expect(ting).to.be.false
+        e.emit('poke')
+        expect(ting).to.be.true
+    })
+
+    it('should ignore emitting an event with no handlers', function () {
+        e.emit('poke')
+        expect(ting).to.be.false
+    })
+
+    it('should provide arguments when emitting an event', function () {
+        e.on('poke', f)
+        expect(ting).to.be.false
+        e.emit('poke', 'mushroom')
+        expect(ting).to.equal('mushroom')
+    })
+
+    it('should remove an event', function () {
+        e.on('poke', f)
+        expect(e.events.poke).to.have.length(1)
+        e.off('poke', f)
+        expect(e.events.poke).to.be.empty
+    })
+
+    it('should remove all events', function () {
+        e.on('poke', f)
+        expect(e.events).to.not.be.empty
+        e.off()
+        expect(e.events).to.be.empty
+    })
+
+    it('should remove all events of a given type', function () {
+        e.on('poke', f)
+        e.on('sway', f)
+        e.off('poke')
+        expect(e.events.poke).to.be.empty
+        expect(e.events.sway).to.have.length(1)
+    })
+})
--- client/test/modules/query_string.js
+++ client/test/modules/query_string.js
@@ -1,1 +1,14 @@
-queryString = require('../../app/modules/query_string')const {expect} = require('chai')describe('query string', () => {    it.skip('needs tests', () =>        expect(false).to.be.true    })})
+const queryString = require('../../app/modules/query_string')
+
+describe('Query String', () =>
+    it('should parse the query string', () => {
+        const qs = queryString.get('a=1&b=test&c=true&d=false&e=1.1')
+        expect(qs).to.deep.equal({
+            a: 1,
+            b: 'test',
+            c: true,
+            d: false,
+            e: 1.1,
+        })
+    })
+)
