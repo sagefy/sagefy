fd381c09872896f760f038b8827db2c42ee283b5
Kevin Heis <heiskr@users.noreply.github.com>
2016-11-03 21:32:52 -0700

    Start move to web pack

---
+++ client/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.json$/,
+            loader: 'json-loader'
+        }, {
+            test: /\.txt$/,
+            loader: 'raw-loader'
+        }]
+    }
+}
