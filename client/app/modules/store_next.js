693fb99e51d45c26f6e8d993d73b540f8079e1dd
Kevin Heis <heiskr@users.noreply.github.com>
2016-11-24 14:11:20 -0800

    Tighten store next

--- client/app/modules/store_next.js
+++ client/app/modules/store_next.js
@@ -1,18 +1,10 @@
 const listeners = []
 let state = window.preload || {}
 let reducer = () => {}
-
-function bind(fn) {
-    listeners.push(fn)
-}
-
-function setReducer(fn) {
-    reducer = fn
-}
-
+function bind(fn) { listeners.push(fn) }
+function setReducer(fn) { reducer = fn }
 function dispatch(action = {type: ''}) {
     state = reducer(state, action)
     listeners.forEach(fn => fn(state, action))
 }
-
 module.exports = {setReducer, bind, dispatch}
