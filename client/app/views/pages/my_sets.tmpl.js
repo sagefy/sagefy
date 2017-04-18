8cdc5322ea1a3f17f56b61d3e168ed6fdc04d1fa
Kevin Heis <heiskr@users.noreply.github.com>
2017-04-18 16:42:00 -0700

    Add info panel to my sets

--- client/app/views/pages/my_sets.tmpl.js
+++ client/app/views/pages/my_sets.tmpl.js
@@ -1,4 +1,5 @@
-const {div, h1, ul, li, h3, p, button, a} = require('../../modules/tags')
+const {div, h1, ul, li, h3, p, button, a,
+       strong, small, br} = require('../../modules/tags')
 // const c = require('../../modules/content').get
 const spinner = require('../components/spinner.tmpl')
 const icon = require('../components/icon.tmpl')
@@ -31,11 +32,16 @@ module.exports = (data) => {
         h1('My Sets'),
         p(
             {className: 'alert--accent'},
-            'Sagefy is new. You will likely find bugs. ',
+            icon('follow'),
+            ' Sagefy is new. You will likely find bugs. ',
+            br(),
             'Please report issues to <support@sagefy.org>. ',
             'Thank you!'
         ),  // TODO-2 Delete this warning message
-        ul(data.userSets.map(set => userSet(set))),
+        ul(
+          {className: 'my-sets__list'},
+          data.userSets.map(set => userSet(set))
+        ),
         data.userSets.length === 0 ? p(
             a(
                 // TODO-2 temporary {href: '/search?mode=as_learner'},
@@ -54,9 +60,44 @@ module.exports = (data) => {
                 icon('search'),
                 ' Find another set'
             )
-        )
+        ),
+        info()
     )
 }
+
+const info = () =>
+  div(
+    {className: 'my-sets__info'},
+    h3('What are cards, units, and sets?'),
+    ul(
+      li(
+        'A ', strong('card'), ' is a single learning activity.',
+        br(),
+        small('(Examples: a 3-minute video or a multiple choice question.)')
+      ),
+      li(
+        'A ', strong('unit'), ' is a single learning goal.',
+        br(), small('(Example: "What is mean, median, and mode?")')
+      ),
+      li(
+        'A ', strong('set'), ' is a collection of units and other sets.',
+        br(),
+        small(
+          '(Like a course, but at any scale. ',
+          'Such as "Measures of Central Tendency", "Intro to Statistics", ',
+          'or even a full degree program.)'
+        )
+      )
+    ),
+    p(
+      'For more details and examples, ',
+      a(
+        {href: 'https://youtu.be/HVwfwTOdnOE'},
+        'check out this 3-minute overview video'
+      ),
+      '.'
+    )
+  )
 
 const userSet = (data) =>
     li(
