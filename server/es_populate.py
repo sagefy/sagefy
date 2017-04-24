72edf8bb63c9c45ef29f8d5ce7523c45239e7ca2
Kevin Heis <heiskr@users.noreply.github.com>
2017-04-24 16:16:37 -0700

    Make es populate run as module or function

--- server/es_populate.py
+++ server/es_populate.py
@@ -5,104 +5,109 @@ from framework.elasticsearch import es
 from modules.util import json_prep, pick
 from database.user import get_avatar
 
-setup_db()
-db_conn = make_db_connection()
 
-# Empty the database
-es.indices.delete(index='entity', ignore=[400, 404])
+def es_populate():
+    setup_db()
+    db_conn = make_db_connection()
 
-# Add users
-users = r.table('users').run(db_conn)
-for user in users:
-    data = pick(json_prep(user), ('id', 'name'))
-    data['avatar'] = get_avatar(user['email'])
-    es.index(
-        index='entity',
-        doc_type='user',
-        body=data,
-        id=user['id'],
-    )
+    # Empty the database
+    es.indices.delete(index='entity', ignore=[400, 404])
 
-# Add units
-units = (r.table('units')
-          .filter(r.row['status'].eq('accepted'))
-          .group('entity_id')
-          .max('created')
-          .default(None)
-          .ungroup()
-          .map(r.row['reduction'])
-          .run(db_conn))
+    # Add users
+    users = r.table('users').run(db_conn)
+    for user in users:
+        data = pick(json_prep(user), ('id', 'name'))
+        data['avatar'] = get_avatar(user['email'])
+        es.index(
+            index='entity',
+            doc_type='user',
+            body=data,
+            id=user['id'],
+        )
 
-for unit in units:
-    es.index(
-        index='entity',
-        doc_type='unit',
-        body=json_prep(unit),
-        id=unit['entity_id'],
-    )
+    # Add units
+    units = (r.table('units')
+              .filter(r.row['status'].eq('accepted'))
+              .group('entity_id')
+              .max('created')
+              .default(None)
+              .ungroup()
+              .map(r.row['reduction'])
+              .run(db_conn))
 
-# Add cards
-cards = (r.table('cards')
-          .filter(r.row['status'].eq('accepted'))
-          .group('entity_id')
-          .max('created')
-          .default(None)
-          .ungroup()
-          .map(r.row['reduction'])
-          .run(db_conn))
-for card in cards:
-    es.index(
-        index='entity',
-        doc_type='card',
-        body=json_prep(card),
-        id=card['entity_id'],
-    )
+    for unit in units:
+        es.index(
+            index='entity',
+            doc_type='unit',
+            body=json_prep(unit),
+            id=unit['entity_id'],
+        )
 
-# Add sets
-sets = (r.table('sets')
-         .filter(r.row['status'].eq('accepted'))
-         .group('entity_id')
-         .max('created')
-         .default(None)
-         .ungroup()
-         .map(r.row['reduction'])
-         .run(db_conn))
-for set_ in sets:
-    es.index(
-        index='entity',
-        doc_type='set',
-        body=json_prep(set_),
-        id=set_['entity_id'],
-    )
+    # Add cards
+    cards = (r.table('cards')
+              .filter(r.row['status'].eq('accepted'))
+              .group('entity_id')
+              .max('created')
+              .default(None)
+              .ungroup()
+              .map(r.row['reduction'])
+              .run(db_conn))
+    for card in cards:
+        es.index(
+            index='entity',
+            doc_type='card',
+            body=json_prep(card),
+            id=card['entity_id'],
+        )
 
-# Add topics
-topics = r.table('topics').run(db_conn)
-for topic in topics:
-    es.index(
-        index='entity',
-        doc_type='topic',
-        body=json_prep(topic),
-        id=topic['id'],
-    )
-
-# Add posts
-posts = r.table('posts').run(db_conn)
-for post in posts:
-    data = json_prep(post)
-    topic = (r.table('topics')
-             .get(data['topic_id'])
+    # Add sets
+    sets = (r.table('sets')
+             .filter(r.row['status'].eq('accepted'))
+             .group('entity_id')
+             .max('created')
+             .default(None)
+             .ungroup()
+             .map(r.row['reduction'])
              .run(db_conn))
-    user = (r.table('users')
-             .get(data['user_id'])
-             .run(db_conn))
-    data['topic'] = json_prep(topic)
-    data['user'] = pick(json_prep(user), ('id', 'name'))
-    es.index(
-        index='entity',
-        doc_type='post',
-        body=data,
-        id=post['id'],
-    )
+    for set_ in sets:
+        es.index(
+            index='entity',
+            doc_type='set',
+            body=json_prep(set_),
+            id=set_['entity_id'],
+        )
+
+    # Add topics
+    topics = r.table('topics').run(db_conn)
+    for topic in topics:
+        es.index(
+            index='entity',
+            doc_type='topic',
+            body=json_prep(topic),
+            id=topic['id'],
+        )
+
+    # Add posts
+    posts = r.table('posts').run(db_conn)
+    for post in posts:
+        data = json_prep(post)
+        topic = (r.table('topics')
+                 .get(data['topic_id'])
+                 .run(db_conn))
+        user = (r.table('users')
+                 .get(data['user_id'])
+                 .run(db_conn))
+        data['topic'] = json_prep(topic)
+        data['user'] = pick(json_prep(user), ('id', 'name'))
+        es.index(
+            index='entity',
+            doc_type='post',
+            body=data,
+            id=post['id'],
+        )
+
+    close_db_connection(db_conn)
 
 
-close_db_connection(db_conn)
+if __name__ == "__main__":
+    es_populate()
