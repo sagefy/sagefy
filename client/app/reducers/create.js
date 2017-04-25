52fd370d202acaa05db5450e4f081c1d56fe27fd
Kevin Heis <heiskr@users.noreply.github.com>
2017-04-24 21:38:14 -0700

    outline create reducer

---
+++ client/app/reducers/create.js
@@ -0,0 +1,22 @@
+/*
+state
+- create:
+— kind
+— step
+— stage (add/create/search, which etc)
+— selectedSet
+    id
+    !!! members
+    ….
+— selectedUnit
+    id
+    !!! requires
+    ….
+— set
+— units
+— cards
+— searchResults
+— body
+
+clear out on success/start new create process
+*/
