9e2dde0c9d437e21c1051a9b2a7b2f9a24ebfa26
Kevin Heis <heiskr@users.noreply.github.com>
2017-05-21 16:02:52 -0700

    Update travis config to include config always

---
+++ config.dev.example.py
@@ -0,0 +1,11 @@
+config = {
+    'debug': True,
+    'mail_sender': 'support@sagefy.org',
+    'mail_password': 'wW6Yd6jJHBVilJHX',
+    'mail_username': 'SMTP_Injection',
+    'mail_server': 'smtp.sparkpostmail.com',
+    'mail_port': 587,
+    'rdb_host': 'localhost',
+    'rdb_port': 28015,
+    'rdb_db': 'sagefy',
+}
