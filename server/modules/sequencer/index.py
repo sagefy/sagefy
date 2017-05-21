fd6c27626bb95fa1329cfb92d092e586f7663c60
Kevin Heis <heiskr@users.noreply.github.com>
2017-05-21 08:38:53 -0700

    Fix two more examples

--- server/modules/sequencer/index.py
+++ server/modules/sequencer/index.py
@@ -9,6 +9,7 @@ from modules.sequencer.params import init_learned
 from database.response import get_latest_response, insert_response
 from time import time
 from schemas.card import assessment_kinds
+from database.card import validate_card_response, score_card_response
 
 """
 Card
@@ -44,11 +45,11 @@ def update(db_conn, user, card, response):
             'feedback': '',
         }
 
-    errors = card.validate_response(response)  # MMM
+    errors = validate_card_response(card, response)
     if errors:
         return {'errors': errors}
 
-    score, feedback = card.score_response(response)  # MMM
+    score, feedback = score_card_response(card, response)
     response = {
         'user_id': user['id'],
         'card_id': card['entity_id'],
