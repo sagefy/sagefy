1ccbb4da9f5873a9d4f662674d212607ba9cb697
Kevin Heis <heiskr@users.noreply.github.com>
2017-09-02 13:25:20 -0700

    conversion script

--- server/dev_data.py
+++ server/dev_data.py
@@ -12,18 +12,8 @@ import os
 from framework.redis import redis
 from datetime import datetime, timezone
 import uuid
-import base64
-
-
-def convert_uuid_to_slug(my_uuid):
-    assert isinstance(my_uuid, uuid.UUID)
-    return base64.urlsafe_b64encode(my_uuid.bytes)[:-2].decode()
-
-
-def convert_slug_to_uuid(slug):
-    assert isinstance(slug, str)
-    slug = slug[0:22]
-    return uuid.UUID(bytes=base64.urlsafe_b64decode(slug + '=='))
+from modules.util import convert_uuid_to_slug
+from database.util import save_row
 
 
 if not config['debug']:
@@ -92,17 +82,16 @@ users = [{
 }]
 
 for user in users:
-    cur = db_conn.cursor()
-    with cur:
-        cur.execute("""
-            INSERT INTO users
-            (  id  ,   created  ,   modified  ,
-               name  ,   email,   password  ,   settings)
-            VALUES
-            (%(id)s, %(created)s, %(modified)s,
-             %(name)s, %(email)s, %(password)s, %(settings)s);
-        """, user)
-        db_conn.commit()
+    query = """
+        INSERT INTO users
+        (  id  ,   created  ,   modified  ,
+           name  ,   email,   password  ,   settings)
+        VALUES
+        (%(id)s, %(created)s, %(modified)s,
+         %(name)s, %(email)s, %(password)s, %(settings)s);
+    """
+    params = user
+    save_row(db_conn, query, params)
 
 
 # We have to translate the sample IDs to UUIDs
@@ -124,150 +113,149 @@ for sample_id, unit_data in sample_data['units'].items():
 
     cur = db_conn.cursor()
     # Dropping foreign key checks so the ordering doesn't matter.
-    with cur:
-        cur.execute("""
-            BEGIN;
-            ALTER TABLE units DISABLE TRIGGER ALL;
-            INSERT INTO units_entity_id
-            (  entity_id  )
-            VALUES
-            (%(entity_id)s);
-            INSERT INTO units
-            (  version_id  ,   created  ,   modified  ,
-               entity_id  ,   previous_id  ,   language  ,   status  ,
-               available  ,   tags  ,   name  ,   user_id  ,
-               body  ,   require_ids  )
-            VALUES
-            (%(version_id)s, %(created)s, %(modified)s,
-             %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
-             %(available)s, %(tags)s, %(name)s, %(user_id)s,
-             %(body)s, %(require_ids)s);
-            ALTER TABLE units ENABLE TRIGGER ALL;
-            COMMIT;
-        """, {
-            'version_id': unit_data['version_id'],
-            'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'entity_id': unit_data['entity_id'],
-            'previous_id': None,
-            'language': 'en',
-            'status': 'accepted',
-            'available': True,
-            'tags': [],
-            'name': unit_data['name'],
-            'user_id': doris_id,
-            'body': unit_data['body'],
-            'require_ids': unit_data['require_ids'],
-        })
-        db_conn.commit()
+    query = """
+        BEGIN;
+        ALTER TABLE units DISABLE TRIGGER ALL;
+        INSERT INTO units_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO units
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           body  ,   require_ids  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(body)s, %(require_ids)s);
+        ALTER TABLE units ENABLE TRIGGER ALL;
+        COMMIT;
+    """
+    params = {
+        'version_id': unit_data['version_id'],
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': unit_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'name': unit_data['name'],
+        'user_id': doris_id,
+        'body': unit_data['body'],
+        'require_ids': unit_data['require_ids'],
+    }
+    save_row(db_conn, query, params)
 
 
 for card_data in sample_data['cards']['video']:
     card_data['entity_id'] = uuid.uuid4()
-    cur = db_conn.cursor()
-    with cur:
-        cur.execute("""
-            INSERT INTO cards_entity_id
-            (  entity_id  )
-            VALUES
-            (%(entity_id)s);
-            INSERT INTO cards
-            (  version_id  ,   created  ,   modified  ,
-               entity_id  ,   previous_id  ,   language  ,   status  ,
-               available  ,   tags  ,   name  ,   user_id  ,
-               unit_id  ,   require_ids  ,   kind  ,   data  )
-            VALUES
-            (%(version_id)s, %(created)s, %(modified)s,
-             %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
-             %(available)s, %(tags)s, %(name)s, %(user_id)s,
-             %(unit_id)s, %(require_ids)s, %(kind)s, %(data)s);
-        """, {
-            'version_id': uuid.uuid4(),
-            'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'entity_id': card_data['entity_id'],
-            'previous_id': None,
-            'language': 'en',
-            'name': 'A Video',
-            'status': 'accepted',
-            'available': True,
-            'tags': [],
-            'user_id': doris_id,
-            'kind': 'video',
-            'data': psycopg2.extras.Json({
-                'site': 'youtube',
-                'video_id': card_data['video_id'],
-            }),
-            'unit_id': entity_ids_lookup[card_data['unit']],
-            'require_ids': [],
-        })
-        db_conn.commit()
+    query = """
+        INSERT INTO cards_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO cards
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           unit_id  ,   require_ids  ,   kind  ,   data  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(unit_id)s, %(require_ids)s, %(kind)s, %(data)s);
+    """
+    params = {
+        'version_id': uuid.uuid4(),
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': card_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'name': 'A Video',
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'user_id': doris_id,
+        'kind': 'video',
+        'data': psycopg2.extras.Json({
+            'site': 'youtube',
+            'video_id': card_data['video_id'],
+        }),
+        'unit_id': entity_ids_lookup[card_data['unit']],
+        'require_ids': [],
+    }
+    save_row(db_conn, query, params)
 
 
 for card_data in sample_data['cards']['choice']:
     card_data['entity_id'] = uuid.uuid4()
     cur = db_conn.cursor()
-    with cur:
-        cur.execute("""
-            INSERT INTO cards_entity_id
-            (  entity_id  )
-            VALUES
-            (%(entity_id)s);
-            INSERT INTO cards
-            (  version_id  ,   created  ,   modified  ,
-               entity_id  ,   previous_id  ,   language  ,   status  ,
-               available  ,   tags  ,   name  ,   user_id  ,
-               unit_id  ,   require_ids  ,   kind  ,   data  )
-            VALUES
-            (%(version_id)s, %(created)s, %(modified)s,
-             %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
-             %(available)s, %(tags)s, %(name)s, %(user_id)s,
-             %(unit_id)s, %(require_ids)s, %(kind)s, %(data)s);
-            INSERT INTO cards_parameters
-            (  id  ,   created  ,   modified,
-              entity_id  ,   guess_distribution  ,   slip_distribution  )
-            VALUES
-            (%(id)s, %(created)s, %(modified)s,
-             %(entity_id)s, %(guess_distribution)s, %(slip_distribution)s);
-        """, {
-            'id': uuid.uuid4(),
-            'version_id': uuid.uuid4(),
-            'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'entity_id': card_data['entity_id'],
-            'previous_id': None,
-            'language': 'en',
-            'name': card_data['body'],
-            'status': 'accepted',
-            'available': True,
-            'tags': [],
-            'user_id': doris_id,
-            'kind': 'choice',
-            'data': psycopg2.extras.Json({
-                'body': card_data['body'],
-                'options': [
-                    {
-                        'id': convert_uuid_to_slug(uuid.uuid4()),
-                        'correct': opt['correct'] == 'Y',
-                        'value': opt['value'],
-                        'feedback': opt['feedback'],
-                    } for opt in card_data['options']
-                ],
-                'order': 'random',
-                'max_options_to_show': 4,
-            }),
-            'unit_id': entity_ids_lookup[card_data['unit']],
-            'require_ids': [],
-            'guess_distribution': psycopg2.extras.Json({
-                str(h): 1 - (0.5 - h) ** 2
-                for h in [h / precision for h in range(1, precision)]
-            }),
-            'slip_distribution': psycopg2.extras.Json({
-                str(h): 1 - (0.25 - h) ** 2
-                for h in [h / precision for h in range(1, precision)]
-            }),
-        })
-        db_conn.commit()
+    query = """
+        INSERT INTO cards_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO cards
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           unit_id  ,   require_ids  ,   kind  ,   data  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(unit_id)s, %(require_ids)s, %(kind)s, %(data)s);
+        INSERT INTO cards_parameters
+        (  id  ,   created  ,   modified,
+          entity_id  ,   guess_distribution  ,   slip_distribution  )
+        VALUES
+        (%(id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(guess_distribution)s, %(slip_distribution)s);
+    """
+    params = {
+        'id': uuid.uuid4(),
+        'version_id': uuid.uuid4(),
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': card_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'name': card_data['body'],
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'user_id': doris_id,
+        'kind': 'choice',
+        'data': psycopg2.extras.Json({
+            'body': card_data['body'],
+            'options': [
+                {
+                    'id': convert_uuid_to_slug(uuid.uuid4()),
+                    'correct': opt['correct'] == 'Y',
+                    'value': opt['value'],
+                    'feedback': opt['feedback'],
+                } for opt in card_data['options']
+            ],
+            'order': 'random',
+            'max_options_to_show': 4,
+        }),
+        'unit_id': entity_ids_lookup[card_data['unit']],
+        'require_ids': [],
+        'guess_distribution': psycopg2.extras.Json({
+            str(h): 1 - (0.5 - h) ** 2
+            for h in [h / precision for h in range(1, precision)]
+        }),
+        'slip_distribution': psycopg2.extras.Json({
+            str(h): 1 - (0.25 - h) ** 2
+            for h in [h / precision for h in range(1, precision)]
+        }),
+    }
+    save_row(db_conn, query, params)
 
 
 for sample_id, subject_data in sample_data['subjects'].items():
@@ -286,39 +274,38 @@ for sample_id, subject_data in sample_data['subjects'].items():
         })
     subject_data['members'] = u_members
 
-    cur = db_conn.cursor()
-    with cur:
-        cur.execute("""
-            INSERT INTO subjects_entity_id
-            (  entity_id  )
-            VALUES
-            (%(entity_id)s);
-            INSERT INTO subjects
-            (  version_id  ,   created  ,   modified  ,
-               entity_id  ,   previous_id  ,   language  ,   status  ,
-               available  ,   tags  ,   name  ,   user_id  ,
-               body  ,   members  )
-            VALUES
-            (%(version_id)s, %(created)s, %(modified)s,
-             %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
-             %(available)s, %(tags)s, %(name)s, %(user_id)s,
-             %(body)s, %(members)s);
-        """, {
-            'version_id': uuid.uuid4(),
-            'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
-            'entity_id': subject_data['entity_id'],
-            'previous_id': None,
-            'language': 'en',
-            'name': subject_data['name'],
-            'status': 'accepted',
-            'available': True,
-            'tags': [],
-            'user_id': doris_id,
-            'body': subject_data['body'],
-            'members': psycopg2.extras.Json(subject_data['members']),
-        })
-        db_conn.commit()
+    query = """
+        INSERT INTO subjects_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO subjects
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           body  ,   members  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(body)s, %(members)s);
+    """
+    params = {
+        'version_id': uuid.uuid4(),
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': subject_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'name': subject_data['name'],
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'user_id': doris_id,
+        'body': subject_data['body'],
+        'members': psycopg2.extras.Json(subject_data['members']),
+    }
+    save_row(db_conn, query, params)
 
 """
 TODO-1 Finish these
--- server/rethinkdb_to_postgresql.py
+++ server/rethinkdb_to_postgresql.py
@@ -3,6 +3,27 @@ import psycopg2
 from modules.util import convert_slug_to_uuid
 from database.util import save_row
 from framework.database import make_db_connection
+from framework.elasticsearch import es
+from framework.redis import redis
+from es_populate import es_populate
+import yaml
+import os
+import psycopg2.extras
+from modules.sequencer.params import precision
+from datetime import datetime, timezone
+import uuid
+from modules.util import convert_uuid_to_slug
+
+dirname = os.path.realpath(__file__).replace('/server/dev_data.py', '')
+
+stream = open(
+    '%s/server/intro_electronic_music_example_collection.yaml' % (dirname,),
+    'r'
+)
+sample_data = yaml.load(stream)
+stream.close()
+
+psycopg2.extras.register_uuid()
 
 r_db_conn = r.connect(
     host='localhost',
@@ -12,9 +33,13 @@ r_db_conn = r.connect(
 )
 pg_db_conn = make_db_connection()
 
-users = r.table('users').run(r_db_conn)
+es.indices.delete(index='entity', ignore=[400, 404])
 
-# users - yes
+redis.flushall()
+
+founder_id = convert_slug_to_uuid('NNKkHsjE3pEOW0wsPaQJm9MD')
+
+users = r.table('users').run(r_db_conn)
 
 for user in users:
     query = """
@@ -26,7 +51,7 @@ for user in users:
     """
     params = {
         'id': convert_slug_to_uuid(user['id']),
-        'created': user['created'],  # TODO need to convert to datetime
+        'created': user['created'],
         'modified': user['modified'],
         'name': user['name'],
         'email': user['email'],
@@ -35,14 +60,220 @@ for user in users:
     }
     save_row(pg_db_conn, query, params)
 
-# TODO generate "dev_data" for cards&params/units/subjects
-# units, subjects, cards/params - probably not TODO check if others exist
-# topics/posts - probably not TODO check if exist
 
-# notices/follows - skip
-# responses - skip
+# We have to translate the sample IDs to UUIDs
+entity_ids_lookup = {}
 
-# users_subjects - intro electronic music... TODO others?
+for sample_id, unit_data in sample_data['units'].items():
+    unit_data['version_id'] = uuid.uuid4()
+    if sample_id in entity_ids_lookup:
+        unit_data['entity_id'] = entity_ids_lookup[sample_id]
+    else:
+        unit_data['entity_id'] = entity_ids_lookup[sample_id] = uuid.uuid4()
+
+    u_require_ids = []
+    for id_ in unit_data['require_ids']:
+        if id_ not in entity_ids_lookup:
+            entity_ids_lookup[id_] = uuid.uuid4()
+        u_require_ids.append(entity_ids_lookup[id_])
+    unit_data['require_ids'] = u_require_ids
+
+    # Dropping foreign key checks so the ordering doesn't matter.
+    query = """
+        BEGIN;
+        ALTER TABLE units DISABLE TRIGGER ALL;
+        INSERT INTO units_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO units
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           body  ,   require_ids  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(body)s, %(require_ids)s);
+        ALTER TABLE units ENABLE TRIGGER ALL;
+        COMMIT;
+    """
+    params = {
+        'version_id': unit_data['version_id'],
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': unit_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'name': unit_data['name'],
+        'user_id': founder_id,
+        'body': unit_data['body'],
+        'require_ids': unit_data['require_ids'],
+    }
+    save_row(pg_db_conn, query, params)
+
+
+for card_data in sample_data['cards']['video']:
+    card_data['entity_id'] = uuid.uuid4()
+    query = """
+        INSERT INTO cards_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO cards
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           unit_id  ,   require_ids  ,   kind  ,   data  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(unit_id)s, %(require_ids)s, %(kind)s, %(data)s);
+    """
+    params = {
+        'version_id': uuid.uuid4(),
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': card_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'name': 'A Video',
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'user_id': founder_id,
+        'kind': 'video',
+        'data': psycopg2.extras.Json({
+            'site': 'youtube',
+            'video_id': card_data['video_id'],
+        }),
+        'unit_id': entity_ids_lookup[card_data['unit']],
+        'require_ids': [],
+    }
+    save_row(pg_db_conn, query, params)
+
+
+for card_data in sample_data['cards']['choice']:
+    card_data['entity_id'] = uuid.uuid4()
+    query = """
+        INSERT INTO cards_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO cards
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           unit_id  ,   require_ids  ,   kind  ,   data  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(unit_id)s, %(require_ids)s, %(kind)s, %(data)s);
+        INSERT INTO cards_parameters
+        (  id  ,   created  ,   modified,
+          entity_id  ,   guess_distribution  ,   slip_distribution  )
+        VALUES
+        (%(id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(guess_distribution)s, %(slip_distribution)s);
+    """
+    params = {
+        'id': uuid.uuid4(),
+        'version_id': uuid.uuid4(),
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': card_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'name': card_data['body'],
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'user_id': founder_id,
+        'kind': 'choice',
+        'data': psycopg2.extras.Json({
+            'body': card_data['body'],
+            'options': [
+                {
+                    'id': convert_uuid_to_slug(uuid.uuid4()),
+                    'correct': opt['correct'] == 'Y',
+                    'value': opt['value'],
+                    'feedback': opt['feedback'],
+                } for opt in card_data['options']
+            ],
+            'order': 'random',
+            'max_options_to_show': 4,
+        }),
+        'unit_id': entity_ids_lookup[card_data['unit']],
+        'require_ids': [],
+        'guess_distribution': psycopg2.extras.Json({
+            str(h): 1 - (0.5 - h) ** 2
+            for h in [h / precision for h in range(1, precision)]
+        }),
+        'slip_distribution': psycopg2.extras.Json({
+            str(h): 1 - (0.25 - h) ** 2
+            for h in [h / precision for h in range(1, precision)]
+        }),
+    }
+    save_row(pg_db_conn, query, params)
+
+
+for sample_id, subject_data in sample_data['subjects'].items():
+    if sample_id in entity_ids_lookup:
+        subject_data['entity_id'] = entity_ids_lookup[sample_id]
+    else:
+        subject_data['entity_id'] = entity_ids_lookup[sample_id] = uuid.uuid4()
+
+    u_members = []
+    for member in subject_data['members']:
+        if member['id'] not in entity_ids_lookup:
+            entity_ids_lookup[member['id']] = uuid.uuid4()
+        u_members.append({
+            'id': convert_uuid_to_slug(entity_ids_lookup[member['id']]),
+            'kind': member['kind'],
+        })
+    subject_data['members'] = u_members
+
+    query = """
+        INSERT INTO subjects_entity_id
+        (  entity_id  )
+        VALUES
+        (%(entity_id)s);
+        INSERT INTO subjects
+        (  version_id  ,   created  ,   modified  ,
+           entity_id  ,   previous_id  ,   language  ,   status  ,
+           available  ,   tags  ,   name  ,   user_id  ,
+           body  ,   members  )
+        VALUES
+        (%(version_id)s, %(created)s, %(modified)s,
+         %(entity_id)s, %(previous_id)s, %(language)s, %(status)s,
+         %(available)s, %(tags)s, %(name)s, %(user_id)s,
+         %(body)s, %(members)s);
+    """
+    params = {
+        'version_id': uuid.uuid4(),
+        'created': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'modified': datetime(2014, 1, 1, tzinfo=timezone.utc),
+        'entity_id': subject_data['entity_id'],
+        'previous_id': None,
+        'language': 'en',
+        'name': subject_data['name'],
+        'status': 'accepted',
+        'available': True,
+        'tags': [],
+        'user_id': founder_id,
+        'body': subject_data['body'],
+        'members': psycopg2.extras.Json(subject_data['members']),
+    }
+
+
+# users_subjects - intro electronic music...
+electronic_subject_eid = entity_ids_lookup['subjectAll']
 for user in users:
     query = """
         INSERT INTO users_subjects
@@ -52,11 +283,11 @@ for user in users:
     """
     params = {
         'user_id': convert_slug_to_uuid(user['id']),
-        'subject_id': '???',  # TODO
+        'subject_id': electronic_subject_eid,
     }
     save_row(pg_db_conn, query, params)
 
+
 r_db_conn.close()
 pg_db_conn.close()
-
-# !!! run !es_populate!
+es_populate()
