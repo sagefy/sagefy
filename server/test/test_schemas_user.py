749621afde27cd6e63ee6c3c684b02828b9a4dde
Kevin Heis <heiskr@users.noreply.github.com>
2017-09-27 18:18:09 -0700

    test_schemas_user

--- server/test/test_schemas_user.py
+++ server/test/test_schemas_user.py
@@ -1,1 +1,6 @@
-import schemas.user  # noqa
+from schemas.user import encrypt_password
+
+
+def test_encrypt_password():
+    assert encrypt_password('abcd').startswith('$2a$')
+    assert encrypt_password('$2a$a') == '$2a$a'
