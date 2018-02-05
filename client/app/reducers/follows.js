76699d86d23f71c47bb831ea731e1242b05ddd7f
Kevin Heis <heiskr@users.noreply.github.com>
2018-02-04 18:41:56 -0800

    Fix follows reducer

--- client/app/reducers/follows.js
+++ client/app/reducers/follows.js
@@ -1,35 +1,32 @@
 const { mergeArraysByKey } = require('../modules/auxiliaries')
 
-module.exports = function follows(state = [], action = { type: '' }) {
+module.exports = function followsReducer(state = [], action = { type: '' }) {
   if (action.type === 'LIST_FOLLOWS_SUCCESS') {
-    const xfollows = mergeArraysByKey(state, action.follows, 'id')
-    return xfollows
+    return mergeArraysByKey(state, action.follows, 'id')
   }
   if (action.type === 'ASK_FOLLOW_SUCCESS') {
     if (action.follows.length === 0) {
       return null
     }
     const follow = action.follows[0]
-    const xfollows = state
-    const index = follows.findIndex(f => f.entity_id === action.entityID)
+    const xfollows = state.slice()
+    const index = xfollows.findIndex(f => f.entity_id === action.entityID)
     if (index > -1) {
-      follows[index] = follow
+      xfollows[index] = follow
     } else {
-      follows.push(follow)
+      xfollows.push(follow)
     }
     return xfollows
-    // TODO-3 will this cause a bug with mergeArraysByKey later?
   }
   if (action.type === 'FOLLOW_SUCCESS') {
-    const xfollows = state
-    follows.push(action.follow)
+    const xfollows = state.slice()
+    xfollows.push(action.follow)
     return xfollows
-    // TODO-3 will this cause a bug with mergeArraysByKey later?
   }
   if (action.type === 'UNFOLLOW_SUCCESS') {
-    const xfollows = state
-    const i = follows.findIndex(follow => follow.id === action.id)
-    follows.splice(i, 1)
+    const xfollows = state.slice()
+    const i = xfollows.findIndex(follow => follow.id === action.id)
+    xfollows.splice(i, 1)
     return xfollows
   }
   return state
