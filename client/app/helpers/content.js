58de3d76d282a6aba476b69316b41ded930c9979
Kevin Heis <heiskr@users.noreply.github.com>
2018-05-26 23:02:45 -0700

    Run prettier

--- client/app/helpers/content.js
+++ client/app/helpers/content.js
@@ -3,7 +3,7 @@ files.en = require('../content/en.json').en
 files.eo = require('../content/eo.json').eo
 
 // Given a key and the language, provide the appropriate content.
-const get = (key, language = 'en') => {
+function get(key, language = 'en') {
   if (!files[language]) {
     return `No Language > ${language}`
   }
--- client/app/helpers/titleize.js
+++ client/app/helpers/titleize.js
@@ -1,7 +1,8 @@
 const snakeCase = require('lodash.snakecase')
 const capitalize = require('lodash.capitalize')
 
-module.exports = function titleize(str = '') { // Turns underscore or camel into title case
+module.exports = function titleize(str = '') {
+  // Turns underscore or camel into title case
   return snakeCase(str)
     .split('_')
     .map(capitalize)
