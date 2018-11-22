7ad8ec7b87b465c39aff61803c4fcc77b142eb3b
Kevin Heis <heiskr@users.noreply.github.com>
2018-11-21 18:40:51 -0800

    More outline

--- _v2/postgres/sagefy.sql
+++ _v2/postgres/sagefy.sql
@@ -5,16 +5,82 @@
 create extension if not exists "uuid-ossp";
 create extension if not exists "pgcrypto";
 
-### Types
+## Users
 
-### Tables
+### Users > Types
 
-### Validations
+### Users > Tables
 
-### Sessions
+### Users > Validations
 
-### Permissions
+### Users > Sessions
 
-### Triggers
+### Users > Permissions
 
-### Capabilities
+### Users > Triggers
+
+### Users > Capabilities
+
+## Topics, Posts, Notices, Follows
+
+### Topics, Posts, Notices, Follows > Types
+
+### Topics, Posts, Notices, Follows > Tables
+
+### Topics, Posts, Notices, Follows > Validations
+
+### Topics, Posts, Notices, Follows > Sessions
+
+### Topics, Posts, Notices, Follows > Permissions
+
+### Topics, Posts, Notices, Follows > Triggers
+
+### Topics, Posts, Notices, Follows > Capabilities
+
+## Cards, Units, Subjects
+
+### Cards, Units, Subjects > Types
+
+### Cards, Units, Subjects > Tables
+
+### Cards, Units, Subjects > Validations
+
+### Cards, Units, Subjects > Sessions
+
+### Cards, Units, Subjects > Permissions
+
+### Cards, Units, Subjects > Triggers
+
+### Cards, Units, Subjects > Capabilities
+
+## User Subjects, Responses
+
+### User Subjects, Responses > Types
+
+### User Subjects, Responses > Tables
+
+### User Subjects, Responses > Validations
+
+### User Subjects, Responses > Sessions
+
+### User Subjects, Responses > Permissions
+
+### User Subjects, Responses > Triggers
+
+### User Subjects, Responses > Capabilities
+
+## Suggests, Suggest Followers
+
+### Suggests, Suggest Followers > Types
+
+### Suggests, Suggest Followers > Tables
+
+### Suggests, Suggest Followers > Validations
+
+### Suggests, Suggest Followers > Sessions
+
+### Suggests, Suggest Followers > Permissions
+
+### Suggests, Suggest Followers > Triggers
+
+### Suggests, Suggest Followers > Capabilities
