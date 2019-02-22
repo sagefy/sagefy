e3e3d532491eb1797d8154c1df72f0cbb42863e4
Kevin Heis <heiskr@users.noreply.github.com>
2019-02-22 15:36:13 -0800

    Make a form errors selector

--- client/index.js
+++ client/index.js
@@ -4,45 +4,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const get = require('lodash.get')
 const request = require('./request')
+const getFormErrors = require('./selectors/form-errors')
 
 require('express-async-errors')
 
 const cacheHash =
   process.env.NODE_ENV === 'test' ? '_' : Date.now().toString(36)
-
-const KNOWN_ERRORS = [
-  {
-    key: 'email_check',
-    field: 'email',
-    message: 'Please format your email like `a@b.c`.',
-  },
-  {
-    key: 'pass_check',
-    field: 'password',
-    message: 'I saw an unfamiliar error.',
-  },
-  {
-    key: 'user_email_key',
-    field: 'email',
-    message: "I've seen this email address before.",
-  },
-  { key: 'user_pkey', message: 'I saw an unfamiliar error.' },
-  {
-    key: 'user_name_key',
-    field: 'name',
-    message: "I've seen this name before.",
-  },
-  {
-    key: 'create_user',
-    message: 'I am unable to send you an email to create your account.',
-  },
-  { key: 'user_user_id_fkey', message: 'I saw an unfamiliar error.' },
-  {
-    key: '355CAC69',
-    field: 'password',
-    message: 'I need at least 8 characters for passwords.',
-  },
-]
 
 const app = express()
 
@@ -77,25 +44,12 @@ app.post('/sign-up', async (req, res) => {
       variables: req.body,
     })
   )
-  if (get(xRes, 'errors')) {
+  const formErrors = getFormErrors(xRes)
+  if (Object.keys(formErrors).length) {
     return res.render('Index', {
       location: req.url,
       cacheHash,
-      formErrors: get(xRes, 'errors')
-        .map(({ message }) => {
-          const found = KNOWN_ERRORS.find(
-            ({ key }) => message.indexOf(key) > -1
-          )
-          return {
-            message: get(found, 'message', message),
-            field: get(found, 'field', undefined),
-          }
-        })
-        .reduce((sum, { message, field }) => {
-          sum[field || 'all'] = sum[field || 'all'] || [] // eslint-disable-line
-          sum[field || 'all'].push(message)
-          return sum
-        }, {}),
+      formErrors,
       prevValues: req.body,
     })
   }
---
+++ client/selectors/form-errors.js
@@ -0,0 +1,54 @@
+const get = require('lodash.get')
+
+const KNOWN_ERRORS = [
+  {
+    key: 'email_check',
+    field: 'email',
+    message: 'Please format your email like `a@b.c`.',
+  },
+  {
+    key: 'pass_check',
+    field: 'password',
+    message: 'I saw an unfamiliar error.',
+  },
+  {
+    key: 'user_email_key',
+    field: 'email',
+    message: "I've seen this email address before.",
+  },
+  { key: 'user_pkey', message: 'I saw an unfamiliar error.' },
+  {
+    key: 'user_name_key',
+    field: 'name',
+    message: "I've seen this name before.",
+  },
+  {
+    key: 'create_user',
+    message: 'I am unable to send you an email to create your account.',
+  },
+  { key: 'user_user_id_fkey', message: 'I saw an unfamiliar error.' },
+  {
+    key: '355CAC69',
+    field: 'password',
+    message: 'I need at least 8 characters for passwords.',
+  },
+]
+
+const DEFAULT_FIELD = 'all'
+
+module.exports = function getFormErrors(xRes) {
+  return get(xRes, 'errors', [])
+    .map(({ message }) => {
+      const found = KNOWN_ERRORS.find(({ key }) => message.indexOf(key) > -1)
+      return {
+        message: get(found, 'message', message),
+        field: get(found, 'field', undefined),
+      }
+    })
+    .reduce((sum, { message, field }) => {
+      const xfield = field || DEFAULT_FIELD
+      sum[xfield] = sum[xfield] || [] // eslint-disable-line
+      sum[xfield].push(message)
+      return sum
+    }, {})
+}
