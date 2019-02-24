3069215bfab54a7d4c4f7356635537ef2c1e8880
Kevin Heis <heiskr@users.noreply.github.com>
2019-02-24 15:28:11 -0800

    Add recommended configs

--- server/index.js
+++ server/index.js
@@ -18,12 +18,38 @@ app.use(
     process.env.DB_SCHEMA,
     {
       // Dev, debug, test
-      graphiql: true,
+      graphiql: process.env.NODE_ENV !== 'production',
 
       // JWT Authentication
       jwtSecret: process.env.JWT_SECRET,
       defaultRole: process.env.JWT_ROLE,
       jwtPgTypeIdentifier: process.env.JWT_TOKEN,
+
+      // Postgraphile recommended config
+      setofFunctionsContainNulls: true,
+      // If none of your RETURNS SETOF compound_type functions
+      // mix NULLs with the results
+      // then you may set this true
+      // to reduce the nullables in the GraphQL schema.
+      ignoreRBAC: false,
+      // Exclude fields, queries and mutations that the
+      // user isn't permitted to access from the generated GraphQL schema.
+      ignoreIndexes: false,
+      // Exclude filters, orderBy, and relations that would
+      // be expensive to access due to missing indexes.
+      dynamicJson: true,
+      // Enables raw JSON input and output.
+      legacyRelations: 'omit',
+
+      // Production config
+      disableQueryLog: process.env.NODE_ENV === 'production',
+
+      // Dev, Test, CI Config
+      showErrorStack: process.env.NODE_ENV !== 'production',
+      extendedErrors:
+        process.env.NODE_ENV === 'production'
+          ? []
+          : ['hint', 'detail', 'errcode'],
     }
   )
 )
