5547adf80c78f2c716890bfa6361882ecea8908b
Kevin Heis <heiskr@users.noreply.github.com>
2019-03-19 16:17:58 -0700

    Update root-get-anon-token.test.js

--- server/test/root-get-anon-token.test.js
+++ server/test/root-get-anon-token.test.js
@@ -1,23 +1,25 @@
-const Joi = require('joi')
+const { assert, object, string } = require('joi')
 const request = require('supertest')
 const { app } = require('../index')
-const { GQL } = require('./_util')
+const {
+  GQL: { rootGetAnonToken },
+} = require('./_util')
 
 describe('root-get-anon-token', () => {
   it('should get an anonymous token', () =>
     request(app)
       .post('/graphql')
       .send({
-        query: GQL.rootGetAnonToken,
+        query: rootGetAnonToken,
         variables: {},
       })
       .expect(({ body }) =>
-        Joi.assert(
+        assert(
           body,
-          Joi.object({
-            data: Joi.object({
-              getAnonymousToken: Joi.object({
-                jwtToken: Joi.string().required(),
+          object({
+            data: object({
+              getAnonymousToken: object({
+                jwtToken: string().required(),
               }),
             }),
           })
