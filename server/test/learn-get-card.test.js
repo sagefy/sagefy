c69e1e3960353e3d5cd3896753785284182ccdaa
Kevin Heis <heiskr@users.noreply.github.com>
2019-04-05 11:58:20 -0700

    Update learn-get-card.test.js

--- server/test/learn-get-card.test.js
+++ server/test/learn-get-card.test.js
@@ -16,6 +16,7 @@ const success = Joi.object({
       kind: Joi.string().required(),
       data: Joi.object(),
     }),
+    selectSubjectLearned: Joi.number().required(),
   }),
 })
 
@@ -34,19 +35,23 @@ describe('learn-get-card', () => {
         query: learnGetCard,
         variables: {
           cardId: data.cards.video.params.entity_id,
+          subjectId: data.subjects.all.entity_id,
         },
       })
       .expect(({ body }) => Joi.assert(body, success))
   })
 
-  it('should allow anyone to read the data', () =>
+  it('should allow anyone to read the data', async () => {
+    const data = await getData()
     request(app)
       .post('/graphql')
       .send({
         query: learnGetCard,
         variables: {
           cardId: '29c8ef2c-ef6b-4690-af24-e0fdf9bd30f4',
+          subjectId: data.subjects.all.entity_id,
         },
       })
-      .expect(({ body }) => Joi.assert(body, fail)))
+      .expect(({ body }) => Joi.assert(body, fail))
+  })
 })
