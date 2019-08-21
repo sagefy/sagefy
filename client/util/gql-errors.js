757e1a6bac45769821091f1a3e48a6de9988b03a
Kevin Heis <heiskr@users.noreply.github.com>
2019-08-21 10:53:21 -0700

    Update gql-errors.js

--- client/util/gql-errors.js
+++ client/util/gql-errors.js
@@ -1,22 +1,22 @@
 const get = require('lodash.get')
 const toPairs = require('lodash.topairs')
 
-const FIELD_REGEXP = /^<(.*?)>/i
+const FIELD_REGEXP = /^<(.*?)>\s?/i
 
 const KNOWN_ERRORS = toPairs({
   email_check: '<email> Please format your email like `a@b.c`.',
   pass_check: '<password> I saw an unfamiliar error.',
   user_email_key: "<email> I've seen this email address before.",
-  user_pkey: ' I saw an unfamiliar error.',
+  user_pkey: 'I saw an unfamiliar error.',
   user_name_key: "<name> I've seen this name before.",
-  create_user: ' I am unable to send you an email to create your account.',
-  user_user_id_fkey: ' I saw an unfamiliar error.',
+  create_user: 'I am unable to send you an email to create your account.',
+  user_user_id_fkey: 'I saw an unfamiliar error.',
   '355CAC69': '<password> I need at least 8 characters for passwords.',
   '51EA51A9': "<password> Your password didn't match.",
   '3883C744': "<email> I didn't find an account with that email.",
   '4F811CFE': "<name> I couldn't find a matching account.",
-  '58483A61': " I couldn't update your email.",
-  EBC6E992: " I couldn't update your password.",
+  '58483A61': "I couldn't update your email.",
+  EBC6E992: "I couldn't update your password.",
   EE05C989: 'No card found.',
   '1306BF1C': 'You may only respond to a scored card.',
   '681942FD': 'You must submit an available response `id`.',
@@ -39,7 +39,7 @@ module.exports = function getGqlErrors(e) {
   if (process.env.NODE_ENV !== 'production') console.error(e) // eslint-disable-line
   return get(e, 'response.errors', [])
     .map(({ message }) =>
-      get(KNOWN_ERRORS.filter(({ key }) => message.indexOf(key) > -1), 0, {
+      get(KNOWN_ERRORS.filter(({ key }) => message.includes(key)), 0, {
         message,
       })
     )
