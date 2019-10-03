b1da5e49e0f225093dbf7375b437df56f387938e
Kevin Heis <heiskr@users.noreply.github.com>
2019-10-03 09:30:53 -0700

    Pg12

--- script/dbbu.sh
+++ script/dbbu.sh
@@ -3,7 +3,7 @@ export $(grep -v '^#' .env | xargs -d '\n')
 cd /var/sagefy/dbbu
 rm *
 today=`date '+%Y_%m_%d__%H_%M_%S'`
-docker exec -it sagefy_postgres_1 pg_dump -U sagefy -a sagefy -p 2600 > "sagefy-$today.sql"
+docker exec -it sagefy_postgres_1 pg_dump -U sagefy -p 2600 > "sagefy-$today.sql"
 ls -al
 b2 authorize_account $B2_ACCOUNT $B2_KEY
 b2 sync /var/sagefy/dbbu b2:sagefy-dbbu
---
+++ script/dbrestore.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# docker exec -it sagefy_postgres_1 pg_dump -U sagefy -p 2600 > sagefy-11.sql
+# docker-compose down
+# mv postgres-data _bu_postgres-data
+# git pull origin master
+# docker-compose up -d --build postgres
+# docker exec -i sagefy_postgres_1 psql -U sagefy -p 2600 < ./postgres/migrations/20190201214238_hello.sql
+# docker exec -i sagefy_postgres_1 psql -U sagefy -p 2600 < sagefy-11.sql
